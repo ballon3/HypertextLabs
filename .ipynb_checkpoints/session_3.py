10a05c48a608837c3110fecddda5e9c250f0b62d
Ryan Brown <ryan393brown@gmail.com>
2018-12-31 17:16:27 -0800

    Sessions podio
    Field sanitizing for spaces and applications

---
+++ .ipynb_checkpoints/session_3.py
@@ -0,0 +1,139 @@
+# coding: utf-8
+for i in resp:
+    app_id = i['app_id']
+    name = i['name']
+
+    
+for i in resp:
+    app_id = i['app_id']
+    name = i['config']['name']
+    print name 
+    print app_id
+    
+get_ipython().magic(u'save session_2 0-52')
+for i in resp:
+    app_id = i['app_id']
+    name = i['config']['name']
+    print name 
+    print app_id
+    
+for i in resp:
+    app_id = i['app_id']
+    name = i['config']['name']
+    app = [a]
+    a = dict(app_id, name)
+   # print name 
+    print app
+for i in resp:
+    app_id = i['app_id']
+    name = i['config']['name']
+    
+    a = dict(app_id, name)
+    app = [a]
+   # print name 
+    print app    for i in resp:
+    app_id = i['app_id']
+    name = i['config']['name']
+    
+    a = dict(app_id, name)
+    app = [a]
+   # print name 
+    print app
+for i in resp:
+    app_id = i['app_id']
+    name = i['config']['name']
+    
+    a = dict(app_id, name)
+    app = [a]
+   # print name 
+    print app
+for i in resp:
+    app_id = i['app_id']
+    name = i['config']['name']
+    
+    a = dict(app_id, name)
+    app = [a]
+   # print name 
+    print app
+for 
+i in resp:
+    i['app_id']
+        name = i['config']['name']
+        
+        a = dict(app_id, name)
+        app = [a]
+       # print name 
+        print app
+for i in resp:
+    app_id = i['app_id']
+    name = i['config']['name']
+    print name 
+    print app_id
+    
+esp = c.Application.find(idx)
+esp = c.Application.find(21896052)
+type(esp)
+esp[fields]
+esp['fields']
+esp['fields']['config']['label']
+fields = esp['fields']
+type(fields)
+json.load(fields)
+json.dump(fields)
+json.dumps(fields)
+raw = json.dumps(fields)
+type(raw)
+raw = json.load(fields)
+raw = json.loads(fields)
+aw = json.loads(raw)
+type(aw)
+type(raw)
+fields = esp['fields']
+raw = json.loads(fields)
+raw = json.dumps(fields)
+type(raw)
+nx = json.loads(raw)
+type(nx)
+nx['config']
+nx[0]
+for i in nx:
+    print i['label']
+    print i['type']
+    
+get_ipython().magic(u'save session_3 50-85')
+get_ipython().magic(u'save session_3 50-90')
+for i in nx:
+    print i['label']
+    print i['type']
+    
+for i in nx:
+    label = i['label']
+    typ = i['type']
+    fid = i['field_id']
+    
+app_fields = []
+flabels = []
+fids = []
+for i in nx:
+    label = i['label']
+    typ = i['type']
+    fid = i['field_id']
+    app_fields.append(fid)
+    flabels.append(label)
+    fids.append(fid)
+    
+print app_fields, fids, flabels
+types = []
+for i in nx:
+    label = i['label']
+    typ = i['type']
+    fid = i['field_id']
+    flabels.append(label)
+    types.append(typ)
+    fids.append(fid)
+    
+print flabels, types, fids
+
+
+
+get_ipython().magic(u'save session_3 50-99')
