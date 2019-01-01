9c65271c09062f15147c5d0707301e74f0448647
BALLON3 <ryan393brown@gmail.com>
2018-12-31 20:36:11 -0800

    Sanitize Fields finally

--- session_1.py
+++ session_1.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 import pprint
 from pypodio2 import api
+import re 
+import string
+import json
 
 
 client_id = "tester-2a2ebd"
@@ -17,8 +20,8 @@ c = api.OAuthClient(
     client_id,
     client_secret,
     username,
-    password,    
-)
+    password,    )
+
 
 """
  c.__dir__ 
@@ -62,34 +65,23 @@ c = api.OAuthClient(
 
 """
 url_n = raw_input("workspace url_label:")
-#org = raw_input("org:")
-def wrkSpace():
 
+def wrkSpace(url):
+    
     url = 'https://podio.com/applegate-solutions/' + url_n
     url2 = 'https://podio.com/hypertext-labs-co/' + url_n
 
-    print(url2)
+    #print(url2)
 
-    space_id = c.Space.find_by_url(url2)
+    space_id = c.Space.find_by_url(url)
 
-    print space_id
+    return space_id
 
 
-#wrkSpace()
-
-"""
- Application(app_id_lst)
-
-(1) iterate through each application 
-(2) for e iter on app - get all_fields
-(3) for e field get type and json struct
-
-"""
-apps = []
-sid = raw_input("space id:")
+sid =wrkSpace(url_n)
 
 def getApps(space_id):
-
+    
     resp = c.Application.list_in_space(space_id)
     apps = []
     ids = []
@@ -100,52 +92,78 @@ def getApps(space_id):
         
         apps.append(name)
         ids.append(app_id)
-        print name 
-        
-    print apps
+        #print name    
+    
+    #return apps
     return ids
 
-#getApps(sid)
 
-ap = raw_input("app id:")
+"""
+ Application(app_id_lst)
 
-def getFields(idx):
+(1) iterate through each application 
+(2) for e iter on app - get all_fields
+(3) for e field get type and json struct
+
+"""
+apps = []
+
+def getAppFields(idx):
 
     #resp = c.Application.get_items(idx)
     #nx = json.loads(raw)
-
-    esp = c.Application.find(idx)
-    nx = esp['fields']
     types = []
     flabels = []
     fids = []
+
+    esp = c.Application.find(idx)
     
+    nx = esp['fields']
+        
     for i in nx:
+        app_f = []
+
         label = i['label']
         typ = i['type']
         fid = i['field_id']
         flabels.append(label)
         types.append(typ)
         fids.append(fid)
+    return types
+    return fids
+    return flabels
         
-    print flabels, types, fids
 
-    """
+    #print flabels, types, fids
+
+
+"""
     merge lists into json obj - sanitize label, lowercase etc..
     once merged each json item should populate two things 
     (1) django model definition 
     (2) 
-    """
-    
-
-
-getFields(ap)
-
-
-
-
 """
-for i in wrks_ndn:
-    print i
-    
-"""
+
+
+def jsonify_App(id):
+    app_list = getApps(id)
+    #print app_list
+    fapp = app_list[0]
+    dt = getAppFields(fapp)
+    apps_obj = [] 
+
+
+    print dt
+
+jsonify_App(sid)
+
+
+class generatePDM(object):
+    pass
+
+
+
+   
+   
+
+
